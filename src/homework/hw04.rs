ede1065b9c150f4010a3aefa031951e61bb68aaa
fastdimer <iidead06@gmail.com>
2026-02-27 11:15:46 +0200

    Practical 4: Grading Students final submission

---
+++ src/homework/hw04.rs
@@ -0,0 +1,30 @@
+#[allow(dead_code)]
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades
+        .iter()
+        .map(|&grade| {
+            if grade < 38 {
+                grade
+            } else {
+                let next_multiple_of_5 = ((grade / 5) + 1) * 5;
+                if next_multiple_of_5 - grade < 3 * 1 {
+                    next_multiple_of_5
+                } else {
+                    grade
+                }
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grading_students() {
+        let input = vec![73, 67, 38, 33];
+        let expected = vec![75, 67, 40, 33];
+        assert_eq!(grading_students(&input), expected);
+    }
+}
